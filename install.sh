b19ee89234be02f0aef554de574c87d4b3a448d7
pyr0ball <pyroballpcs@gmail.com>
2023-08-08 23:40:28 -0700

    refactor of dependency resolution system also stripped old text and unused options

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@
 
 # initial vars
 VERSION=0.0.1
-script_title="Slipstream Foundation SDK Installer - v$VERSION"
 installdir="/opt/SlipstreamSDK"
 
 # Escape characters (if your shell uses a different one, you can modify it here)
@@ -24,39 +23,52 @@ esac
 scriptname="${BASH_SOURCE[0]##*/}"
 rundir="${BASH_SOURCE[0]%/*}"
 
+# Store functions revision in a variable to compare with if already installed
+installer_functionsrev=$functionsrev
+required_functionsrev=1.6.0
+
 # Source PRbL Functions locally or retrieve from online
-if [ ! -z $prbl_functions ] ; then
-    source $prbl_functions
-else
-    if [ -f ${rundir}/functions ] ; then
-        source ${rundir}/functions
-    else
+if [ -n "$prbl_functions" ]; then
+    source "$prbl_functions"
+    vercompare=$(vercomp "$installer_functionsrev" "$required_functionsrev")
+    if [[ $vercompare == 2 ]]; then
         prbl_functions_url='https://raw.githubusercontent.com/pyr0ball/PRbL/main/functions'
         # Iterate through get commands and fall back on next if unavailable
-        if command -v curl >/dev/null 2>&1; then
-            source <(curl -ks $prbl_functions_url )
-        elif command -v wget >/dev/null 2>&1; then
-            source <(wget -qO- $prbl_functions_url )
-        elif command -v fetch >/dev/null 2>&1; then
-            source <(fetch -qo- $prbl_functions_url )
-        else
-            echo "Error: curl, wget, and fetch commands are not available. Please install one to retrieve PRbL functions."
-            exit 1
-        fi
+        for cmd in "curl" "wget" "fetch"; do
+            if command -v "$cmd" >/dev/null 2>&1; then
+                source <("$cmd" -ks "$prbl_functions_url")
+                break
+            fi
+        done
+    fi
+else
+    if [ -f "${rundir}/functions" ]; then
+        source "${rundir}/functions"
+    else
+        prbl_functions_url='https://raw.githubusercontent.com/pyr0ball/PRbL/main/functions'
+        for cmd in "curl" "wget" "fetch"; do
+            if command -v "$cmd" >/dev/null 2>&1; then
+                source <("$cmd" -ks "$prbl_functions_url")
+                break
+            fi
+        done
     fi
 fi
 
+script_title="$(center "Slipstream Foundation SDK Installer - v$VERSION")"
 rundir_absolute=$(pushd $rundir ; pwd ; popd)
 escape_dir=$(printf %q "${rundir_absolute}")
 logfile="${rundir}/${pretty_date}_${scriptname}.log"
+# Set light blue borders
+brdr=${gry}
 
+# Pull in the versios file
+source $rundir/VERSIONS
 
 #-----------------------------------------------------------------#
 # Script-specific Parameters
 #-----------------------------------------------------------------#
 
-# Store functions revision in a variable to compare with if already installed
-installer_functionsrev=$functionsrev
 
 # Get and store the user currently executing this script
 runuser=$(whoami)
@@ -77,9 +89,9 @@ backup_files=()
 installed_files=()
 installed_dirs=()
 
-# List of dependency packaged to be istalled via apt (For Debian/Ubuntu)
-packages=()
-packages=(
+# Dependencies
+sys_packages=()
+sys_packages=(
     git
     curl
     python3
@@ -87,13 +99,11 @@ packages=(
     cmake
     libgmp3-dev
     libgtk-3-dev
-    venv
-    wget
 )
 
 pip_packages=()
 pip_packages=(
-    pyparsing
+    pyparsing==3.0.9
     gitpython
 )
 
@@ -106,7 +116,7 @@ repo_packages=(
 
 prbl_packages=()
 prbl_packages=(
-    golang.19.x.install
+    golang.install
 )
 
 # This variable is what is injected into the bashrc
@@ -139,16 +149,16 @@ fi
 
 script-title(){
     boxborder \
-"${ss1}           ___          _    _                                        " \
-"${ss2}          |       |         | |           |                           " \
-"${ss3}           -+-    +     +   |-       -   -+-  |-     -     -    |- -  " \
-"${ss4}              |   |     |   |        \    |   |     |/    | |   | | | " \
-"${ss5}           ---    -                  -     -         --    --         " \
-"${ss6}                                ___   ___                             " \
-"${ss7}                               |       | | |  /                       " \
-"${ss8}                                -+-    + | |-+                        " \
-"${ss9}                                   |   | | |  \                       " \
-"${ss10}                                ---   ---                             "
+"${ss1}           ___          _    _                                        ${dfl}" \
+"${ss2}          |       |         | |           |                           ${dfl}" \
+"${ss3}           -+-    +     +   |-       -   -+-  |-     -     -    |- -  ${dfl}" \
+"${ss4}              |   |     |   |        \    |   |     |/    | |   | | | ${dfl}" \
+"${ss5}           ---    -                  -     -         --    --         ${dfl}" \
+"${ss6}                                ___   ___                             ${dfl}" \
+"${ss7}                               |       | | |  /                       ${dfl}" \
+"${ss8}                                -+-    + | |-+                        ${dfl}" \
+"${ss9}                                   |   | | |  \                       ${dfl}" \
+"${ss10}                                ---   ---                            ${dfl}"
 }
 
 # Function for displaying the usage of this script
@@ -182,26 +192,21 @@ detectvim(){
 
 check-deps(){
     # Iterate through the list of required packages and check if installed
-    for pkg in ${sys_packages[@]} ; do
-        #local _pkg=$(dpkg -l $pkg 2>&1 >/dev/null ; echo $?)
-        
-        # If not installed, add it to the list of missing bins
-        if ! check-packages $pkg ; then
+    for pkg in "${sys_packages[@]}" ; do
+        pkg_installed=$(check-packages $pkg)
+        if ! $pkg_installed ; then
             bins_missing+=($pkg)
         fi
     done
-    pybin=$(which python3)
-    if [ -z $pybin ] ; then
+    if ! command -v python3 >/dev/null 2>&1 ; then
         bins_missing+=(python3)
-        # TODO: make a universal package install function
     fi
-    pipbin=$(which pip3)
-    if [ -z $pipbin ] ; then
+
+    if ! command -v pip3 >/dev/null 2>&1 ; then
         bins_missing+=(pip3)
     else
-        for pkg in ${pip_packages[@]} ; do
-            pippkg_installed=$(pip list | grep -F $pkg ; echo $?)
-            if [[ $pippkg_installed != 0 ]] ; then
+        for pkg in "${pip_packages[@]}" ; do
+            if ! pip show "$pkg" >/dev/null 2>&1 ; then
                 bins_missing+=("pip: $pkg")
             fi
         done
@@ -209,8 +214,7 @@ check-deps(){
 
     # This installer requires golang to work
     # TODO: better handling of prbl_packages as dependencies
-    gobin=$(which go)
-    if  [ -z $gobin ] ; then
+    if ! command -v go >/dev/null 2>&1 ; then
         bins_missing+=("prbl: golang.install")
     fi
     # Count the number of entries in bins_missing
@@ -225,20 +229,23 @@ check-deps(){
 
 install-deps(){
     logger echo "${grn}Installing packages:${lyl} $sys_packages${dfl}"
-    for _package in ${sys_packages[@]} ; do
-        run install-packages $_package
+    for _package in "${sys_packages[@]}" ; do
+        install_result=$(run install-packages "$_package")
+        if [[ $install_result != 0 ]] ; then
+            warn "Package install for $_package failed"
+        fi
     done
-    if [ -f ${rundir}/requirements.txt ] ; then
+    if [ -f "${rundir}/requirements.txt" ] ; then
         logger echo "${grn}Installing pip requirements$...${dfl}"
-        run pip install -U ${rundir}/requirements.txt
+        run pip install -U -r "${rundir}/requirements.txt"
     else
-        if [ ! -z $pip_packages ] ; then
+        if [ ! -z "$pip_packages" ] ; then
             logger echo "${grn}Installing pip packages:${lyl} $pip_packages${dfl}"
-            run pip install -U ${pip_packages[@]}
+            run pip install -U "${pip_packages[*]}"
         fi
     fi
     logger echo "${grn}Checking Repositories:${lyl} $repo_packages${dfl}"
-    pushd $installdir
+    pushd "$installdir"
         for repo in "${repo_packages[@]}"; do
             if ! check-git-repository "repositories/${repo#*/}"; then
                 if [ -d "repositories/${repo#*/}" ] ; then
@@ -263,6 +270,21 @@ install-deps(){
     popd
     # Sets dependency installed flag to true
     depsinstalled=true
+}
+
+dry-run-report(){
+    box-rounded
+    boxborder "${grn}Dry-run Report:${dfl}"
+    box-norm
+    boxborder \
+    "bins_missing= " \
+    "${bins_missing[@]}" \
+    "backup_files= " \
+    "${backup_files[@]}" \
+    "installed_files= " \
+    "${installed_files[@]}" \
+    "installed_dirs= " \
+    "${installed_dirs[@]}"
 }
 
 install-functions(){
@@ -296,10 +318,10 @@ install-prbl(){
         if [[ "${result[idx]}" == "true" ]] ; then
             if [[ $dry_run != true ]] ; then
                 boxline "running extra $extra"
-                run "${escape_dir}/extras/$extra -i"
+                run "${rundir_absolute}/extras/$extra -i"
             else
                 dry_run=false
-                run "${escape_dir}/extras/$extra -D"
+                run "${rundir_absolute}/extras/$extra -D"
                 dry_run=true
             fi
         # else
@@ -398,7 +420,7 @@ export s3dk_functions=\"${installdir}/functions\""
 }
 
 install(){
-    # Create global install directory
+    # Create install directory
     run mkdir -p ${installdir}
 
     install-functions
@@ -418,7 +440,7 @@ install(){
             0)  boxline "${grn}Installing dependencies...${dfl}"
                 install-deps
                 ;;
-            1)  warn "Dependent Utilities missing: $bins_missing" ;;
+            1)  warn "Skipping install of Dependent Utilities: $bins_missing" ;;
         esac
     fi
 
@@ -474,14 +496,12 @@ remove-arbitrary(){
 }
 
 update(){
-    remove-arbitrary
-    run git stash -m "$pretty_date stashing changes before update to latest"
-    run git fetch && run git pull --recurse-submodules
-    pushd PRbL
-        run git checkout main
-        run git pull
+    #remove-arbitrary
+    pushd $installdir
+        run git stash -m "$pretty_date stashing changes before update to latest"
+        run git fetch && run git pull --recurse-submodules
     popd
-    install
+    #install
 }
 
 dry-run-report(){
@@ -504,49 +524,10 @@ dry-run-report(){
 #------------------------------------------------------#
 
 #$ Options Parser
-diags(){
-    script-title
-    boxborder \
-        "Diagnostics:" \
-        "Local IP: $local_ip" \
-        "Cert File: $cert_file" \
-        "Key File: $key_file" \
-        "Target(s): $target"
-}
 
 while [[ $# -gt 0 ]] ; do
-    key="$1"
+    arg2="$1"
     case $key in
-        -c | --cert-file)
-            if [[ -n $2 && ! $2 == -* ]]; then
-                cert_file=$2
-                shift
-            else
-                warn "Invalid argument for $key: $2"
-                usage
-                exit 1
-            fi
-            ;;
-        -k | --key-file)
-            if [[ -n $2 && ! $2 == -* ]]; then
-                key_file=$2
-                shift
-            else
-                warn "Invalid argument for $key: $2"
-                usage
-                exit 1
-            fi
-            ;;
-        -t | --target)
-            if [[ -n $2 && ! $2 == -* ]]; then
-                target=$2
-                shift
-            else
-                warn "Invalid argument for $key: $2"
-                usage
-                exit 1
-            fi
-            ;;
         -D | --dry-run)
             export dry_run=true
             install
@@ -559,12 +540,10 @@ while [[ $# -gt 0 ]] ; do
             ;;
         -u | --update)
             export update_run=true
-            diags
-            update && unset update_run && success " Certificate $cert_file and Key $key_file ${lyl}Updated${dfl}]"
+            update && unset update_run && success " $script_title ${lyl}Updated${dfl}]"
             ;;
         -i | --install)
-            diags
-            install && success " Certificate $cert_file and Key $key_file ${lyl}Installed${dfl}]"
+            install && success " $script_title ${lyl}Installed${dfl}]"
             ;;
         -F | --force-remove)
             remove-arbitrary && success " $script_title ${lyl}Force-Removed${dfl}]"
@@ -573,16 +552,17 @@ while [[ $# -gt 0 ]] ; do
             usage
             ;;
         -*)
-            warn "Invalid option: $key"
+            warn "Invalid option: $arg2"
             usage
             exit 1
             ;;
         *)
-            if [[ -f $key ]]; then
+            if [[ -f $arg2 ]]; then
                 # handle filename passed with flag
-                process_file $key
+                # process_file $arg2
+                break
             else
-                warn "Invalid argument: $key"
+                warn "Invalid argument: $arg2"
                 usage
                 exit 1
             fi
